0f71fa0edfe302fc536a041e068fa42fb23f537a
James <james@jemz.org>
2017-01-06 14:40:16 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var epxress = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+app.get ('/', function(req, res){
+	res.send('metadata project');
+});
+
+app.listen(port, function(){
+	console.log('metadata app listening on port ' + port);
+});
